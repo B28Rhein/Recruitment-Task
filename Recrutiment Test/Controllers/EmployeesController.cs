af47ec4a7156e5e12fb50ce0b2647aa27522764c
Sebastian Matejak <sebastian.mat.2201@gmail.com>
2024-06-21 10:00:26 +0200

    Employees are now editable

--- Recrutiment Test/Controllers/EmployeesController.cs
+++ Recrutiment Test/Controllers/EmployeesController.cs
@@ -36,12 +36,12 @@ namespace Recrutiment_Test.Controllers
             return View(await context.Employees.ToListAsync());
         }
         [HttpPost]
-        public async Task<IActionResult> Index(string fullName, string subdivision, string position, string Status, int Id, int oooBalance)
+        public async Task<IActionResult> Index(string fullName, string subdivision, string position, string Status, int peoplesPartner, int oooBalance)
         {
             ViewData["Positions"] = positions;
             ViewData["Subdivisions"] = subdivisions;
             bool Active = Status == "on" ? true : false;
-            Employee.AddNewEmployee(fullName, subdivision, position, Active, Id, oooBalance, context);
+            Employee.AddNewEmployee(fullName, subdivision, position, Active, peoplesPartner, oooBalance, context);
             return View(await context.Employees.ToListAsync());
         }
         public IActionResult AddEmployee()
@@ -64,6 +64,92 @@ namespace Recrutiment_Test.Controllers
             ViewBag.Position = new SelectList(positions);
             ViewBag.Subdivision = new SelectList(subdivisions);
             return View(employeeModel);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await context.Employees.FindAsync(id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            EmployeeModel employeeModel = new EmployeeModel();
+            employeeModel.EmployeeList = new List<SelectListItem>();
+
+            var data = context.Employees.ToList();
+            foreach (var item in data)
+            {
+                if (item.Position == 0)
+                {
+                    employeeModel.EmployeeList.Add(new SelectListItem
+                    {
+                        Text = item.FullName,
+                        Value = item.Id.ToString()
+                    });
+                }
+            }
+
+            ViewBag.Position = new SelectList(positions);
+            ViewBag.Subdivision = new SelectList(subdivisions);
+            ViewData["EmployeeModel"] = employeeModel;
+            return View(Employee);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, string fullName, string subdivision, string position, bool Status, int peoplesPartner, int oooBalance)
+        {
+            Employee employee = new Employee()
+            {
+                Id = id,
+                FullName = fullName,
+                Subdivision = subdivisions.IndexOf(subdivision),
+                Position = positions.IndexOf(position),
+                Status = Status,
+                PeoplePartner = peoplesPartner,
+                OutOfOfficeBalance = oooBalance
+            };
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(employee);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (context.Employees.Find(employee.Id) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewBag.Position = new SelectList(positions);
+            ViewBag.Subdivision = new SelectList(subdivisions);
+            EmployeeModel employeeModel = new EmployeeModel();
+            employeeModel.EmployeeList = new List<SelectListItem>();
+
+            var data = context.Employees.ToList();
+            foreach (var item in data)
+            {
+                if (item.Position == 0)
+                {
+                    employeeModel.EmployeeList.Add(new SelectListItem
+                    {
+                        Text = item.FullName,
+                        Value = item.Id.ToString()
+                    });
+                }
+            }
+            return View(new object[] { employee, employeeModel });
         }
     }
 }
